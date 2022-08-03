8892b786898d492ba6e44047b896aba4069f4c2e
Harald Vinje <harald@xperitech.com>
2022-08-03 16:13:07 +0200

    Latest

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 
-export default function Counter({ initialCount = 0 }: { initialCount?: number }) {
+export default function Counter({
+  initialCount = 0
+}: {
+  initialCount?: number
+}) {
   const [count, setCount] = useState(initialCount)
   const increment = () => setCount(count + 1)
   const decrement = () => setCount(count - 1)
@@ -8,22 +12,25 @@ export default function Counter({ initialCount = 0 }: { initialCount?: number })
   const buttonStyle =
     'text-white font-bold w-12 py-2 px-4 rounded transition ease-in-out'
 
-  const resetButtonStyle = " rounded border border-blue-500 bg-transparent w-32 py-2 my-2 font-semibold text-blue-700 transition ease-in-out hover:border-transparent hover:bg-blue-500 hover:text-white"
+  const resetButtonStyle =
+    ' rounded border border-blue-500 bg-transparent w-32 py-2 my-2 font-semibold text-blue-700 transition ease-in-out hover:border-transparent hover:bg-blue-500 hover:text-white'
 
   return (
     <>
       <p className="text-2xl">Counter</p>
       <div className="my-4 flex w-32 justify-between">
         <button
-          aria-label='decrement'
+          aria-label="decrement"
           className={`${buttonStyle} bg-red-500 hover:bg-red-600`}
           onClick={decrement}
         >
           -
         </button>
-        <span data-cy="counter" className="text-2xl">{count}</span>
+        <span data-cy="counter" className="text-2xl">
+          {count}
+        </span>
         <button
-          aria-label='increment'
+          aria-label="increment"
           className={`${buttonStyle} bg-green-400 hover:bg-green-500`}
           onClick={increment}
         >
@@ -31,14 +38,14 @@ export default function Counter({ initialCount = 0 }: { initialCount?: number })
         </button>
       </div>
       <button
-        aria-label='initial'
+        aria-label="initial"
         className={resetButtonStyle}
         onClick={() => setCount(initialCount)}
       >
         Reset
       </button>
       <button
-        aria-label='zero'
+        aria-label="zero"
         className={resetButtonStyle}
         onClick={() => setCount(0)}
       >
